ed4ce7ddd9fe64335b7ae6513519100065df8365
Daulet <daulet.tusupbaev@gmail.com>
2025-01-16 16:48:46 +0500

    feat: add material route generator
    Added a route generator to manage navigation within the application.
    
    This includes:
    
    - Creating a `RouteGenerator` class to handle route generation.
    - Defining routes for the home screen and add employee screen.
    - Implementing an error route for unknown routes.
    - Integrating the route generator into the main application widget.

---
+++ lib/route/route_generator.dart
@@ -0,0 +1,33 @@
+import 'package:employee_drift_book/screens/add_employee_screen.dart';
+import 'package:employee_drift_book/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
+      case 'add_employee':
+        return MaterialPageRoute(builder: (_) => const AddEmployeeScreen());
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('No route'),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text(
+            'Sorry no route was found!',
+            style: TextStyle(fontSize: 18, color: Colors.red),
+          ),
+        ),
+      );
+    });
+  }
+}
