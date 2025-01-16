50f8e01d754ec76323921cc8f16aaff217807b20
Daulet <daulet.tusupbaev@gmail.com>
2025-01-16 15:44:11 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# employee_drift_book
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/dev/dtussupbay/employee_drift_book/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.dtussupbay.employee_drift_book
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
