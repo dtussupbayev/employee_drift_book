a0266fa1569059e13d88ef05f717f22e4a7f18db
Daulet <daulet.tusupbaev@gmail.com>
2025-01-17 13:46:53 +0500

    feat: display employees list from database
    Displays a list of employees fetched from the local database on the home screen.
    
    - Implemented a FutureBuilder to asynchronously retrieve employee data.
    - Used a ListView.builder to dynamically display employees in a card format.
    - Updated UI to display employee details like ID, username, first name, last name, and date of birth.
    - Included error handling and loading indicators for better user experience.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,27 @@
+import 'package:employee_drift_book/data/local/db/app_db.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late AppDb _db;
+
+  @override
+  void initState() {
+    super.initState();
+    _db = AppDb();
+  }
+
+  @override
+  void dispose() {
+    _db.close();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,12 +30,68 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Home'),
         centerTitle: true,
       ),
-      body: const Column(
-        children: [],
-      ),
+      body: FutureBuilder<List<EmployeeData>>(
+          future: _db.getEmployees(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            final List<EmployeeData>? employees = snapshot.data;
+            if (snapshot.hasData && employees != null) {
+              return ListView.builder(
+                itemCount: employees.length,
+                itemBuilder: (context, index) {
+                  final employee = employees[index];
+                  return GestureDetector(
+                    onTap: () {},
+                    child: Card(
+                      color: Colors.grey.shade300,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(
+                          color: Colors.grey.shade400,
+                          style: BorderStyle.solid,
+                          width: 1.2,
+                        ),
+                        borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(16.0),
+                          bottomRight: Radius.circular(16.0),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              employee.id.toString(),
+                            ),
+                            Text(
+                              employee.userName.toString(),
+                            ),
+                            Text(
+                              employee.firstName.toString(),
+                            ),
+                            Text(
+                              employee.lastName.toString(),
+                            ),
+                            Text(
+                              employee.dateOfBirth.toString(),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          }),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
-          Navigator.pushNamed(context, '/add_employee');
+          Navigator.pushNamed(context, '/add_employee').then(
+            (value) => setState(() {}),
+          );
         },
         icon: const Icon(Icons.add),
         label: const Text('Add employee'),
